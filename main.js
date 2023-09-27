e3889c14f933930c4a38bd23d62b89a9e6fdc87c
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2023-09-27 14:58:50 +0100

    Add EmailJS integration for sending contact form emails

---
+++ main.js
@@ -0,0 +1,35 @@
+// Initialize EmailJS with your user ID
+emailjs.init("Z-Hc8MBkJw47ijVD8");
+
+// Function to send email when the form is submitted
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Get the user input from the form
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  // Use EmailJS to send the email
+  emailjs
+    .send("service_lrjjc55", "template_r2fh64h", {
+      from_name: name,
+      from_email: email,
+      message: message,
+    })
+    .then(
+      function (response) {
+        console.log("Email sent successfully", response);
+        // You can add code here to show a success message to the user
+
+        // Clear the form inputs
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+      },
+      function (error) {
+        console.error("Email send failed", error);
+        // You can add code here to show an error message to the user
+      }
+    );
+});
