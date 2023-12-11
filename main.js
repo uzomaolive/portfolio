0582c0c38132810fdec84c95416eb23fe01121ca
olive uzoma <olive.c.uzoma@gmail.com>
2023-12-11 17:58:16 +0100

    fixing the scroll to the top button

--- main.js
+++ main.js
@@ -33,3 +33,22 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
       }
     );
 });
+
+//scroll to top function
+document.addEventListener("DOMContentLoaded", function () {
+  let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  window.addEventListener("scroll", function () {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          scrollToTopBtn.style.display = "block";
+      } else {
+          scrollToTopBtn.style.display = "none";
+      }
+  });
+
+  scrollToTopBtn.addEventListener("click", function () {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+  });
+});
+
